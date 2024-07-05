9e1dea14a1f4e62cda7e5e0e9aa097a821d8181f
Seunghyun-Kang <kiseyno@gmail.com>
2024-07-05 12:57:21 +0900

    Updated ip-adapter model code

--- stable_diffusion/image_generator.py
+++ stable_diffusion/image_generator.py
@@ -1,20 +1,23 @@
-# from ip_adapter_face import IPAdapterFace
-# from stable_diffusion_xl import StableDiffusionXL
+
+from stable_diffusion_xl import StableDiffusionXL
 from aura import ImageUpscaler
 from ip_adapter_face import FaceIDGenerator
 
-image_url = "base1.jpg"
+image_url = "base.jpg"
+negative_prompt = "disfigured, ugly, bad, immature, cartoon, anime, 3d, painting, b&w, extra head, extra legs, extra arms, extra body"
 
-# input_image_path = "./base.jpg"
 # output_image_path = "./output/generated_image.png"
 # prompt = "Make her stand up, big tits, same lighting and high-end camera quality, rim lighting, looking at the camera, ultra quality, sharp focus, tack sharp, dof, film grain, 8K UHD, high detailed skin, high detailed skin, skin pores, like real insta-worthy"
-# negative_prompt = "disfigured, ugly, bad, immature, cartoon, anime, 3d, painting, b&w, extra head, extra legs, extra arms, extra body"
+
 
 # # 1. image generator
 # sd_xl = StableDiffusionXL()
-# sd_xl.generate_image(prompt, negative_prompt,
+# refined_image = sd_xl.generate_image(prompt, negative_prompt,
 #                      num_inference_steps=30, high_noise_frac=0.8)
 
+# 이미지 저장
+# refined_image.save(output_image_path)
+# print(f"Image saved at {output_image_path}")
 
 # ////////////////////////////////////////////////////////////////////////
 # IPAdapterFace 클래스 초기화
@@ -29,40 +32,33 @@ image_url = "base1.jpg"
 #                           num_inference_steps=30, output_image_path=output_image_path)
 
 # ////////////////////////////////////////////////////////////////////////
-# Usage
+#Upscaler usage
 
-output_image_path = f"./output/upscaled_{image_url}"
+# output_image_path = f"./output/upscaled_{image_url}"
 
-upscaler = ImageUpscaler()
-image = upscaler.load_image_from_file(image_url)
-upscaled_image = upscaler.upscale_image(image)
+# upscaler = ImageUpscaler()
+# image = upscaler.load_image_from_file(image_url)
+# upscaled_image = upscaler.upscale_image(image)
 
 
-upscaled_image.save(output_image_path)
-print(f"Image saved at {output_image_path}")
+# upscaled_image.save(output_image_path)
+# print(f"Image saved at {output_image_path}")
 
 #//////////////////////////////////////////////////////////////////////////////
-image_paths = [
-    "./ip_adapter_face_id/face/1.jpg", 
-    "./ip_adapter_face_id/face/2.jpg", 
-    "./ip_adapter_face_id/face/3.jpg", 
-    "./ip_adapter_face_id/face/4.jpg", 
-    "./ip_adapter_face_id/face/5.jpg"
-]
+#Adapter usage
+
+face_model = "jiwon"
 
 generator = FaceIDGenerator()
-faceid_embeds = generator.get_face_embeddings(image_paths)
+faceid_embeds = generator.get_face_embeddings(face_model)
 
-prompt = "photo of a woman in red dress in a garden"
-negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, blurry"
+prompt = "photo of Asian girl with black eyes, wearing dress at the beach"
 
 images = generator.generate_images(
     prompt=prompt, 
     negative_prompt=negative_prompt, 
     faceid_embeds=faceid_embeds, 
-    num_samples=4, 
-    width=512, 
-    height=512, 
+    num_samples=4,
     num_inference_steps=30, 
     seed=2023
 )
--- stable_diffusion/ip_adapter_face.py
+++ stable_diffusion/ip_adapter_face.py
@@ -11,50 +11,64 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from stable_diffusion.ip_adapter_face_id.ip_adapter import ip_adapter_faceid_separate 
 from stable_diffusion.ip_adapter_face_id.insightface.app import face_analysis
 
-app = face_analysis.FaceAnalysis(name="buffalo_l", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
-app.prepare(ctx_id=0, det_size=(640, 640))
+class FaceIDGenerator:
+    def __init__(self, device="cuda"):
+        self.device = device
+        self.app = face_analysis.FaceAnalysis(name="buffalo_l", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+        self.app.prepare(ctx_id=0, det_size=(640, 640))
+        base_model_path = "SG161222/Realistic_Vision_V4.0_noVAE"
+        vae_model_path = "stabilityai/sd-vae-ft-mse"
+        ip_ckpt = "./ip_adapter_face_id/ip-adapter-faceid-portrait_sd15.bin"
+        
+        self.noise_scheduler = DDIMScheduler(
+            num_train_timesteps=1000,
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            clip_sample=False,
+            set_alpha_to_one=False,
+            steps_offset=1,
+        )
+        
+        self.vae = AutoencoderKL.from_pretrained(vae_model_path).to(dtype=torch.float16)
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            base_model_path,
+            torch_dtype=torch.float16,
+            scheduler=self.noise_scheduler,
+            vae=self.vae,
+            feature_extractor=None,
+            safety_checker=None
+        )
+        
+        self.ip_model = ip_adapter_faceid_separate.IPAdapterFaceID(self.pipe, ip_ckpt, self.device, num_tokens=16, n_cond=5)
 
-images = ["./ip_adapter_face_id/face/1.jpg", "./ip_adapter_face_id/face/2.jpg", "./ip_adapter_face_id/face/3.jpg", "./ip_adapter_face_id/face/4.jpg", "./ip_adapter_face_id/face/5.jpg"]
+    def get_face_embeddings(self, face_model):
+        default_face_id_paths = "./ip_adapter_face_id/face_id_sample/"
+        folder_path = os.path.join(default_face_id_paths, face_model)
+        if not os.path.exists(folder_path):
+            raise FileNotFoundError(f"지정된 폴더 경로가 존재하지 않습니다: {folder_path}")
 
-faceid_embeds = []
-for image in images:
-    image = cv2.imread('base.jpg')
-    faces = app.get(image)
-    faceid_embeds.append(torch.from_numpy(faces[0].normed_embedding).unsqueeze(0).unsqueeze(0))
-faceid_embeds = torch.cat(faceid_embeds, dim=1)
+        image_extensions = ['.jpg', '.jpeg', '.png']
+        image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.splitext(f)[1].lower() in image_extensions]
+        if len(image_paths) == 0:
+            raise FileNotFoundError(f"지정된 폴더에 얼굴 데이터가 없습니다: {folder_path}")
 
-base_model_path = "SG161222/Realistic_Vision_V4.0_noVAE"
-vae_model_path = "stabilityai/sd-vae-ft-mse"
-ip_ckpt = "ip-adapter-faceid-portrait_sd15.bin"
-device = "cuda"
+        faceid_embeds = []
+        for image_path in image_paths:
+            image = cv2.imread(image_path)
+            faces = self.app.get(image)
+            if faces:
+                faceid_embeds.append(torch.from_numpy(faces[0].normed_embedding).unsqueeze(0).unsqueeze(0))
+        return torch.cat(faceid_embeds, dim=1)
 
-noise_scheduler = DDIMScheduler(
-    num_train_timesteps=1000,
-    beta_start=0.00085,
-    beta_end=0.012,
-    beta_schedule="scaled_linear",
-    clip_sample=False,
-    set_alpha_to_one=False,
-    steps_offset=1,
-)
-vae = AutoencoderKL.from_pretrained(vae_model_path).to(dtype=torch.float16)
-pipe = StableDiffusionPipeline.from_pretrained(
-    base_model_path,
-    torch_dtype=torch.float16,
-    scheduler=noise_scheduler,
-    vae=vae,
-    feature_extractor=None,
-    safety_checker=None
-)
-
-
-# load ip-adapter
-ip_model = ip_adapter_faceid_separate.IPAdapterFaceID(pipe, ip_ckpt, device, num_tokens=16, n_cond=5)
-
-# generate image
-prompt = "photo of a woman in red dress in a garden"
-negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, blurry"
-
-images = ip_model.generate(
-    prompt=prompt, negative_prompt=negative_prompt, faceid_embeds=faceid_embeds, num_samples=4, width=512, height=512, num_inference_steps=30, seed=2023
-)
+    def generate_images(self, prompt, negative_prompt, faceid_embeds, num_samples=4, width=512, height=512, num_inference_steps=30, seed=2023):
+        return self.ip_model.generate(
+            prompt=prompt, 
+            negative_prompt=negative_prompt, 
+            faceid_embeds=faceid_embeds, 
+            num_samples=num_samples, 
+            width=width, 
+            height=height, 
+            num_inference_steps=num_inference_steps, 
+            seed=seed
+        )
